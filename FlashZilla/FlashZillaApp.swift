d78667322eb9fe2ddebba96a4669a96dc6d52954
David Lee <david.lee.seungjin@gmail.com>
2022-06-24 12:47:45 -0600

    Initial Commit

---
+++ FlashZilla/FlashZillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashZillaApp.swift
+//  FlashZilla
+//
+//  Created by David Lee on 6/24/22.
+//
+
+import SwiftUI
+
+@main
+struct FlashZillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
