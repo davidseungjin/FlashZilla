e0d11f7fd680a8b1cef1b117d210f924ff87b4dd
David Lee <david.lee.seungjin@gmail.com>
2022-06-25 11:06:29 -0600

    different card color action according to left to right or right to left

--- FlashZilla/CardView.swift
+++ FlashZilla/CardView.swift
@@ -9,13 +9,14 @@ import SwiftUI
 
 struct CardView: View {
   let card: Card
+  var removal: (()->Void)? = nil
   @State private var isShowingAnswer = false
+  @State private var offset = CGSize.zero
   
   var body: some View {
     ZStack {
       RoundedRectangle(cornerRadius: 25, style: .continuous)
-        .fill(.white)
-        .shadow(radius: 10)
+        fill.(offset.width > 0 ? .green : .red)
       VStack {
         Text(card.prompt)
           .font(.largeTitle)
@@ -32,6 +33,22 @@ struct CardView: View {
       .multilineTextAlignment(.center)
     }
     .frame(width: 450, height: 250)
+    .rotationEffect(.degrees(Double(offset.width / 5)))
+    .offset(x: offset.width * 5, y: 0)
+    .opacity(2 - Double(abs(offset.width / 50)))
+    .gesture(
+        DragGesture()
+            .onChanged { gesture in
+                offset = gesture.translation
+            }
+            .onEnded { _ in
+                if abs(offset.width) > 100 {
+                    // remove the card
+                } else {
+                    offset = .zero
+                }
+            }
+    )
     .onTapGesture {
       isShowingAnswer.toggle()
     }
--- FlashZilla/ContentView.swift
+++ FlashZilla/ContentView.swift
@@ -10,12 +10,25 @@ import CoreHaptics
 
 struct ContentView: View {
   @State private var cards = [Card](repeating: Card.example, count: 10)
+  @State private var offset = CGSize.zero
+  
+  func removeCard(at index: Int) {
+    cards.remove(at: index)
+  }
+  
       var body: some View {
         ZStack {
+          Image("background")
+            .resizable()
+            .ignoresSafeArea()
           VStack {
             ZStack {
               ForEach(0..<cards.count, id: \.self) { index in
-                CardView(card: Card.example)
+                CardView(card: Card.example) {
+                  withAnimation {
+                    removeCard(at: index)
+                  }
+                }
                   .stacked(at: index, in: cards.count)
               }
             }
